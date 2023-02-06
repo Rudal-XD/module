68b065d3a37cdedb0e27daa3250d9b4d77bf0267
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-06 09:27:26 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,42 +1,43 @@
-import os,time
+import requests, json, time, re, os
 
-class menu:
-	def __init__(self):
-		os.system('git pull')
-		os.system('clear')
-		print("""
-		1.install python         [ON]
-		2.install requests	 [ON]
-		3.install mechanize	 [ON]
-		4.exit""")
-		usna = input('nomor :')
-		if usna == ['']:
-			print('masukan nomor yg bener')
-		elif usna == ['1']:
-			os.system('xdg-open https://linktr.ee/mikaz_')
-			try:
-				import python
-				print('python telah terinstall')
-			except ImportError:
-				print('sedang install python...')
-				os.system('pkg install python')
-				sleep.time(1);menu()
-		elif usna == ['2']:
-			try:
-				import requests
-				print('requeats telah terinstall')
-			except ImportError:
-				print('sedang install requests...')
-				os.system('pip install requests')
-		elif usna == ['3']:
-			try:
-				import mechanize
-				print('mechanize telah terinstall')
-			except ImportError:
-				print('sedang install mechanize...')
-				os.system('pip install mechanize')
-		elif usna == ['4']:
-			print('terima kasih telah mengunakan tools kami');sleep.time(1);exit()
-			
-if __name__=='__main__':
-	menu()
+# Warna
+KUNING = ('\x1b[1;93m')
+MERAH = ('\x1b[1;91m')
+HIJAU = ('\x1b[1;92m')
+PUTIH = ('\x1b[1;97m')
+# Banner
+banner = (f"""     {MERAH}({PUTIH}-rozhak-{MERAH}){PUTIH}
+{MERAH}╔═╗┌─┐┌┐┌┬  ┬┌─┐┬─┐┌┬┐
+{MERAH}║  │ ││││└┐┌┘├┤ ├┬┘ │
+{PUTIH}╚═╝└─┘┘└┘ └┘ └─┘┴└─ ┴
+""")
+# Convert Cookie Ke Token
+class convert:
+
+  def __init__(self):
+    os.system('clear')
+    print(f"""{banner}
+{HIJAU}1.{PUTIH} Mendapatkan token EAAI
+{HIJAU}2.{PUTIH} Dapatkan token EAAB
+{HIJAU}3.{PUTIH} Cara menggunakan
+{HIJAU}4.{PUTIH} Keluar {HIJAU}({MERAH}exit{HIJAU}){MERAH}
+   """)
+    masuk = input(f"{KUNING}?.{PUTIH} Choose :{HIJAU} ")
+    if masuk == '1' or masuk == '01':
+      cookie = input(f"\n{HIJAU}?.{PUTIH} Cookie :{KUNING} ")
+      if 'c_user=' in str(cookie):
+        self.__satu__(cookie)
+      else:
+        exit(f"{MERAH}!.{MERAH} Periksa cookienya")
+    elif masuk == '2' or masuk == '02':
+      cookie = input(f"\n{HIJAU}?.{PUTIH} Cookie :{KUNING} ")
+      if 'c_user=' in str(cookie):
+        self.__dua__(cookie)
+      else:
+        exit(f"{MERAH}!.{MERAH} Periksa cookienya")
+    elif masuk == '3' or masuk == '03':
+      print(f"{KUNING}?.{PUTIH} Anda akan diarahkan ke youtube...");time.sleep(3);os.system('xdg-open https://youtu.be/Xv0plMtS4Cw');exit()
+    elif masuk == '4' or masuk == '04':
+      exit()
+    else:
+      exit(f"{MERAH}!.{MERAH} Wrong input")
