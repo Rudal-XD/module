9d5a4c669d46f9d514a3ffa82fce73382094e1c4
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-06 12:25:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-import requests, json, time, re, os
+import requests, time, os, re, json, random
+from rich.panel import Panel
+from rich import print
+from concurrent.futures import ThreadPoolExecutor
+from rich.tree import Tree
+from rich.console import Console
 
-# Warna
-KUNING = ('\x1b[1;93m')
-MERAH = ('\x1b[1;91m')
-HIJAU = ('\x1b[1;92m')
-PUTIH = ('\x1b[1;97m')
 # Banner
-banner = (f"""{MERAH}({HIJAU}˜”*°•.˜”*°• Mikaz •°*”˜.•°*”˜{MERAH}){PUTIH}
-{MERAH}================================
-{MERAH}|{HIJAU} [98]. {KUNING}WA SAYA LANGSUNG	|
-{MERAH}|{HIJAU} [99]. {KUNING}MY INFORMASI		|
-{MERAH}================================
+banner = (f"""[bold red]([bold green]˜”*°•.˜”*°• Mikaz •°*”˜.•°*”˜[bold red])
+[bold blue]================================
+| [bold yellow][98]. [bold green]WA SAYA LANGSUNG[bold blue]	|
+| [bold yellow][99]. [bold green]MY INFORMASI		[bold blue]|
+[bold blue]================================
 """)
 # Convert Cookie Ke Token
 class convert:
@@ -18,18 +18,18 @@ class convert:
   def __init__(self):
     os.system('clear')
     print(f"""{banner}|				|
-|          {HIJAU}==  MENU  =={MERAH}		|
-{MERAH}================================
-{HIJAU}[1].{PUTIH} install python
-{HIJAU}[2].{PUTIH} Install requests
-{HIJAU}[3].{PUTIH} install mechanize
-{HIJAU}[4].{PUTIH} Keluar {HIJAU}({MERAH}exit{HIJAU}){MERAH}
-{MERAH}================================
+|          [bold red]==  MENU  ==[bold blue]		|
+[bold blue]================================
+| [bold yellow][1].[bold green] install python[bold blue]		|
+[bold blue]| [bold yellow][2].[bold green] Install requests[bold blue]	 	|
+[bold blue]| [bold yellow][3].[bold green] install mechanize[bold blue]	|
+[bold blue]| [bold yellow][4].[bold red] Keluar (exit)[bold white]	[bold blue]	|
+[bold blue]================================
    """)
-    masuk = input(f"{KUNING}?.{PUTIH} masukan nomor :{HIJAU} ")
+    masuk = input(f"?.masukan nomor :")
     if masuk == '98' or masuk == '098':
       try:
-      	os.system('xdg-open https://wa.me/qr/C7AKGGQIHROWM1')
+      	os.system('xdg-open https://wa.me/qr/C7AKGGQIHROWM')
       except IOError:
       	print('LINK ERROR')
     elif masuk == '99' or masuk == '099':
@@ -61,7 +61,7 @@ class convert:
     elif masuk == '4' or masuk == '04':
       exit()
     else:
-      print(f"{MERAH}!.{MERAH}----- Masukan nomor yang bener-----");time.sleep(3);convert()
+      print(f"[bold red]!.[bold red]----- Masukan nomor yang bener-----[bold white]");time.sleep(3);convert()
 
 if __name__=='__main__':
    convert()
